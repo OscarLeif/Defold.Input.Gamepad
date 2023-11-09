ca587ebf56bfadd8855cea5ee578f986dca8dd72
Oscar Fernandez <oscar.leif@gmail.com>
2023-11-09 00:30:01 -0500

    Just testing.
    The first thing to do is.
    Call a Toast Message.

---
+++ nativeplugin/src/ext_toast.cpp
@@ -0,0 +1,135 @@
+// Extension lib defines
+#define EXTENSION_NAME androidnative
+#define LIB_NAME "androidnative"
+#define MODULE_NAME LIB_NAME
+
+
+// Defold SDK
+#define DLIB_LOG_DOMAIN LIB_NAME
+#include <dmsdk/sdk.h>
+
+#if defined(DM_PLATFORM_ANDROID)
+
+//#include "testlib.h" // Multiply
+
+static JNIEnv* Attach()
+{
+    JNIEnv* env;
+    JavaVM* vm = dmGraphics::GetNativeAndroidJavaVM();
+    vm->AttachCurrentThread(&env, NULL);
+    return env;
+}
+
+static bool Detach(JNIEnv* env)
+{
+    bool exception = (bool) env->ExceptionCheck();
+    env->ExceptionClear();
+    JavaVM* vm = dmGraphics::GetNativeAndroidJavaVM();
+    vm->DetachCurrentThread();
+    return !exception;
+}
+
+namespace {
+    struct AttachScope
+    {
+        JNIEnv* m_Env;
+        AttachScope() : m_Env(Attach())
+        {
+        }
+        ~AttachScope()
+        {
+            Detach(m_Env);
+        }
+    };
+}
+
+static jclass GetClass(JNIEnv* env, const char* classname)
+{
+    jclass activity_class = env->FindClass("android/app/NativeActivity");
+    jmethodID get_class_loader = env->GetMethodID(activity_class,"getClassLoader", "()Ljava/lang/ClassLoader;");
+    jobject cls = env->CallObjectMethod(dmGraphics::GetNativeAndroidActivity(), get_class_loader);
+    jclass class_loader = env->FindClass("java/lang/ClassLoader");
+    jmethodID find_class = env->GetMethodID(class_loader, "loadClass", "(Ljava/lang/String;)Ljava/lang/Class;");
+
+    jstring str_class_name = env->NewStringUTF(classname);
+    jclass outcls = (jclass)env->CallObjectMethod(cls, find_class, str_class_name);
+    env->DeleteLocalRef(str_class_name);
+    return outcls;
+}
+
+static int DoStuffJava(lua_State* L)
+{
+    return 1;
+}
+
+// Functions exposed to Lua
+static const luaL_reg Module_methods[] =
+{
+    {"dostuff_java", DoStuffJava},
+    //{"dostuff_jar", DoStuffJar},
+    //{"vibrate", Vibrate},
+    //{"getraw", GetRaw},
+    //{"multiply", Multiply},
+    {0, 0}
+};
+
+static void LuaInit(lua_State* L)
+{
+    int top = lua_gettop(L);
+
+    // Register lua names
+    luaL_register(L, MODULE_NAME, Module_methods);
+
+    lua_pop(L, 1);
+    assert(top == lua_gettop(L));
+}
+
+static dmExtension::Result AppInitializeExtension(dmExtension::AppParams* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result InitializeExtension(dmExtension::Params* params)
+{
+    // Init Lua
+    LuaInit(params->m_L);
+    printf("Registered %s Extension\n", MODULE_NAME);
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result AppFinalizeExtension(dmExtension::AppParams* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result FinalizeExtension(dmExtension::Params* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+#else
+
+static dmExtension::Result AppInitializeExtension(dmExtension::AppParams* params)
+{
+    dmLogWarning("Registered %s (null) Extension\n", MODULE_NAME);
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result InitializeExtension(dmExtension::Params* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result AppFinalizeExtension(dmExtension::AppParams* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result FinalizeExtension(dmExtension::Params* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+#endif
+
+DM_DECLARE_EXTENSION(EXTENSION_NAME, LIB_NAME, AppInitializeExtension, AppFinalizeExtension, InitializeExtension, 0, 0, FinalizeExtension)
