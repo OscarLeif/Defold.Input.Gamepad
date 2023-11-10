7e7837d6eba59b157e029895bda48331be8e8579
Oscar Fernandez <oscar.leif@gmail.com>
2023-11-10 00:33:35 -0500

    Update AndroidGamepad.java

--- nativeplugin/src/android/AndroidGamepad.java
+++ nativeplugin/src/android/AndroidGamepad.java
@@ -11,6 +11,7 @@ import androidx.core.content.ContextCompat;
 import android.content.pm.PackageManager;
 import android.util.Log;
 
+import android.view.View;
 import android.view.KeyEvent;
 
 public class AndroidGamepad extends Fragment
@@ -30,5 +31,32 @@ public class AndroidGamepad extends Fragment
 		super.onCreate(savedInstanceState);
 		setRetainInstance(true);
 	}
+
+     @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.setFocusableInTouchMode(true);
+        view.requestFocus();
+        view.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    // 处理返回按键事件
+                    return true; // 消费掉按键事件
+                }
+                return false;
+            }
+        });
+    }
+
+    private boolean isSupportedKey(int keyCode) {
+        // Check if the key code is in the list of supported keys
+        for (int supportedKey : SUPPORTED_KEYS) {
+            if (keyCode == supportedKey) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
